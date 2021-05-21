56d54e1673c396a18a6eec21839b5f3e57dfa716
Aman Pareek <aman.pareek@wolterskluwer.com>
2021-05-21 07:07:37 +0530

    Day-13

---
+++ Day-13/prefixMatchingBinary.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include<climits>
+using namespace std;
+int main(){
+    string s1 ="aa";
+    string s2 = "b"
+    cout<<s1.compare(0,s1.length(),s2,0,s1.length());
+}
